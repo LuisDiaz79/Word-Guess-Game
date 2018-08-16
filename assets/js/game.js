b446da87167c4f12137b72dd3ccf1206d0bed0fc
Luis <hhavocs@yahoo.com>
2018-08-16 19:45:52 -0400

    some colors

--- assets/js/game.js
+++ assets/js/game.js
@@ -39,8 +39,6 @@ document.onkeyup = function(event) {
     userText = userText.trim().toUpperCase();
     var livesTag = document.getElementById("liveTag");
     var lives = parseInt(livesTag.innerText);
-    console.dir(livesTag);
-    console.log(userText);
     var keycode = event.keyCode ? event.keyCode : event.charCode;
     var image = document.getElementById("imageletter");
     if(keycode>=65 && keycode<=90){
@@ -60,86 +58,88 @@ document.onkeyup = function(event) {
     var imagesrc = "live"+lives;
     var skull = document.getElementById(imagesrc);
 
-    if( arrayLength== 0){
-        if(keycode>=65 && keycode<=90){
-            values = userText;
-            keyList.push(userText);
-            valid=true;
-        }else{
-            valid=false;
-        }
-    }else if (keycode>=65 && keycode<=90) {
-        if(values.indexOf(userText) <0){
-            values+=","+userText;
-            var valList = values.split(",");
-            valList.sort();
-            values = valList;
-            keyList.push(userText);
-            valid=true;
-        }else{
-            valid=false;
-        }
-    }else{
-        valid=false;
-    }
-    lettersTag.textContent = values;
-
-    if(valid){
-        hiddenword ="";
-        var count =0;
-        for (var i = 0;  i < word.length; i++){
-            var element =word[i]; 
-            if(element==userText){
-                console.log("HERE  " + i);
-                hiddenword += userText + " ";
-                console.log(hiddenword);
-                count++;
+    if(lives>0 && hiddenword.indexOf("_") >-1){
+        if( arrayLength== 0){
+            if(keycode>=65 && keycode<=90){
+                values = userText;
+                keyList.push(userText);
+                valid=true;
             }else{
-                if (/^[a-zA-Z]+$/.test(element)) {
-                    if(keyList.indexOf(element)>-1){
-                        hiddenword += element + " ";
-                    }else{
-                        hiddenword += "_ ";
-                    }
-                }else if(element == " "){
-                    hiddenword += "&nbsp ";
-                    if((hiddenword.length/17)>1 && (hiddenword.length/17)<2){
-                        hiddenword += "<br />";
-                    }
-                }else{
-                    count++;
-                    hiddenword += element + " ";
-                }
+                valid=false;
             }
-            console.log("hiddenword : " + hiddenword);
-        }   
-        var winword = document.getElementById("word");
-        winword.innerHTML = hiddenword; 
-    }
-    if(count>0){
-        var audio = document.getElementById("answer");
-        audio.play();
-    }
-    if(count==0){
-        skull.style="display:inline;"
-        lives--;
-        livesTag.textContent = lives;
+        }else if (keycode>=65 && keycode<=90) {
+            if(values.indexOf(userText) <0){
+                values+=","+userText;
+                var valList = values.split(",");
+                valList.sort();
+                values = valList;
+                keyList.push(userText);
+                valid=true;
+            }else{
+                valid=false;
+            }
+        }else{
+            valid=false;
+        }
+        lettersTag.textContent = values;
+    
+        if(valid){
+            hiddenword ="";
+            var count =0;
+            for (var i = 0;  i < word.length; i++){
+                var element =word[i]; 
+                if(element==userText){
+                    hiddenword += userText + " ";
+                    count++;
+                }else{
+                    if (/^[a-zA-Z]+$/.test(element)) {
+                        if(keyList.indexOf(element)>-1){
+                            hiddenword += element + " ";
+                        }else{
+                            hiddenword += "_ ";
+                        }
+                    }else if(element == " "){
+                        hiddenword += "&nbsp ";
+                        if((hiddenword.length/17)>1 && (hiddenword.length/17)<2){
+                            hiddenword += "<br />";
+                        }
+                    }else{
+                        count++;
+                        hiddenword += element + " ";
+                    }
+                }
+            }   
+            var winword = document.getElementById("word");
+            winword.innerHTML = hiddenword; 
+        }
+        if(count>0){
+            var audio = document.getElementById("answer");
+            audio.play();
+        }
+        if(count==0){
+            skull.style="display:inline;"
+            lives--;
+            livesTag.textContent = lives;
+            
+            $("#liveTagDiv").fadeOut("fast");
+            $("#liveTagDiv").fadeIn("fast");
+            var audio = document.getElementById("wrong-answer");
+            audio.play();
+        }
         
-        $("#liveTagDiv").fadeOut("fast");
-        $("#liveTagDiv").fadeIn("fast");
-        var audio = document.getElementById("wrong-answer");
-        audio.play();
+        if(hiddenword.indexOf("_") <0){
+            var win = document.getElementById("win");
+            win.style="display:inline;"
+        }
+        if(lives<1){
+            var gameover = document.getElementById("gameover");
+            gameover.style="display:inline;"
+            var audio = document.getElementById("lose");
+            audio.play();
+        }else if(lives <3){
+            liveTagDiv.style="color:red;";
+        }
     }
     
-    if(hiddenword.indexOf("_") <0){
-        var win = document.getElementById("win");
-        win.style="display:inline;"
-    }
-    if(lives<1){
-        var gameover = document.getElementById("gameover");
-        gameover.style="display:inline;"
-        var audio = document.getElementById("lose");
-        audio.play();
-    }
     console.log(word);  
 }
